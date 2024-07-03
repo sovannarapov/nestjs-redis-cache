2e695a42ee964c4468a3324397144ef11f762511
sovannarapov <sovannarapov9@gmail.com>
2024-07-03 13:59:59 +0700

    feat: :sparkles: complete implement fetch orders

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { groupBy } from 'lodash';
+import { HttpClientService } from 'src/http-client/http-client.service';
+import { UtilService } from 'src/util/redis.service';
+import { OrderDto } from './order.dto';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+  constructor(
+    private http: HttpClientService,
+    private cache: UtilService,
+  ) {}
+
+  async getAllOrders(category: string, skip: number, take: number) {
+    const key = `OrderService:orders:group:${category}`;
+
+    const cached = await this.cache.readListFromCache(
+      key,
+      skip,
+      take + skip - 1,
+    );
+
+    if (cached && cached.length > 0) {
+      console.log('cache found');
+      return cached.map((element) => JSON.parse(element));
+    } else {
+      console.log('cache missed');
+      const data = await this.http.getAllOrders();
+      const groupedData = this.groupedData(data);
+      await this.cache.cacheList(key, groupedData, 600);
+      console.log('cached succeed');
+      return groupedData;
+    }
+  }
+
+  private groupedData(orders: OrderDto[]) {
+    const groupedOrders = [].concat(
+      [],
+      ...orders
+        .map((order) =>
+          groupBy(
+            order['orderLines'],
+            (orderLine) =>
+              `${orderLine.orderNumber}||${orderLine.storeLocation}||${orderLine.deliveryLocation?.name}||${orderLine.item.mode.config.name}`,
+          ),
+        )
+        .map((order) =>
+          Object.keys(order).map((key) => ({ key, data: order[key] })),
+        ),
+    );
+
+    return groupedOrders;
+  }
+}
