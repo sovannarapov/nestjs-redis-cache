612acf9f6246a5f2554494481dc94319896ea2d8
sovannarapov <sovannarapov9@gmail.com>
2024-07-03 13:53:53 +0700

    feat: :sparkles: implement http-client service

--- src/http-client/http-client.service.ts
+++ src/http-client/http-client.service.ts
@@ -1,4 +1,18 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class HttpClientService {}
+export class HttpClientService {
+  constructor(private http: HttpService) {}
+
+  async getAllOrders() {
+    const ordersObs = this.http.get(
+      'https://run.mocky.io/v3/598d1999-d56f-4c25-92a6-36f3ec51c8e9',
+    );
+
+    return await (
+      await firstValueFrom(ordersObs)
+    ).data;
+  }
+}
