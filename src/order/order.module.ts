31f56a25899938ee70633d7a78a3d5ab23d71535
sovannarapov <sovannarapov9@gmail.com>
2024-07-03 14:01:12 +0700

    refactor: :recycle: update import module

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { AppModule } from 'src/app.module';
 
 @Module({
+  imports: [forwardRef(() => AppModule)],
   controllers: [OrderController],
   providers: [OrderService],
 })
