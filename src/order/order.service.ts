105f4b2be85534f647242c5831d0a782f446576c
Sovannara Pov <sovannarapov9@gmail.com>
2024-07-04 23:38:04 +0700

    refactor: :truck: improve naming variables

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -25,15 +25,19 @@ export class OrderService {
       return cached.map((element: string) => JSON.parse(element));
     } else {
       console.log('cache missed');
-      const data = await this.http.getAllOrders();
-      const groupedData = this.groupedData(data);
+      
+      const orders = await this.http.getAllOrders();
+      const groupedOrders = this.groupOrders(orders);
+      
       await this.cache.cacheList(key, groupedData, 600);
+      
       console.log('cached succeed');
-      return groupedData;
+      
+      return groupedOrders;
     }
   }
 
-  private groupedData(orders: OrderDto[]) {
+  private groupOrders(orders: OrderDto[]) {
     const groupedOrders = [].concat(
       [],
       ...orders
