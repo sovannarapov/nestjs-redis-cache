0538d8f22173c1889cea63e60ec5f03ee6afa9ce
sovannarapov <sovannarapov9@gmail.com>
2024-07-04 23:46:49 +0700

    Revert "refactor: :truck: improve naming variables"
    This reverts commit 105f4b2be85534f647242c5831d0a782f446576c.

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -25,19 +25,15 @@ export class OrderService {
       return cached.map((element: string) => JSON.parse(element));
     } else {
       console.log('cache missed');
-      
-      const orders = await this.http.getAllOrders();
-      const groupedOrders = this.groupOrders(orders);
-      
+      const data = await this.http.getAllOrders();
+      const groupedData = this.groupedData(data);
       await this.cache.cacheList(key, groupedData, 600);
-      
       console.log('cached succeed');
-      
-      return groupedOrders;
+      return groupedData;
     }
   }
 
-  private groupOrders(orders: OrderDto[]) {
+  private groupedData(orders: OrderDto[]) {
     const groupedOrders = [].concat(
       [],
       ...orders
